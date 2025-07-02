14f4448be8b9a88a58cbe31dfaeb459cdcd35ee5
extremelygood <extremelygood@vacant.com>
2025-07-02 08:11:16 +0200

    Added expiring object

---
+++ app/src/main/java/com/extremelygood/abfahrt/classes/Expiring.kt
@@ -0,0 +1,28 @@
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.time.Duration
+
+interface Expiring {
+    fun startExpiring(duration: Duration, onExpired: () -> Unit = {})
+    fun cancelExpiring()
+}
+
+class ExpiringDelegate(
+    private val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
+) : Expiring {
+
+    private var job: Job? = null
+
+    override fun startExpiring(duration: Duration, onExpired: () -> Unit) {
+        job?.cancel()
+        job = scope.launch {
+            delay(duration)
+            onExpired()
+        }
+    }
+
+    override fun cancelExpiring() { job?.cancel(); job = null }
+}
