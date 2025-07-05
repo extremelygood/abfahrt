98523dda050ffe97eb51beb704c097108e79c418
extremelygood <h.matschigow@gmail.com>
2025-07-05 08:28:33 +0200

    Skeleton for encounterhandler and how it interacts with a connection

--- app/src/main/java/com/extremelygood/abfahrt/network/EncounterHandler.kt
+++ app/src/main/java/com/extremelygood/abfahrt/network/EncounterHandler.kt
@@ -1,15 +1,47 @@
 package com.extremelygood.abfahrt.network
 
+import com.extremelygood.abfahrt.network.packets.ParsedCombinedPacket
+import com.extremelygood.abfahrt.network.packets.RequestProfilePacket
+
 /**
  * Class for handling a connection made to another device
  */
 class EncounterHandler(
     private val connection: NearbyConnection
 ) {
+    init {
+        connection.setDisconnectCallback {
+            end()
+        }
+
+        connection.setPacketReceiveCallback { packet ->
+            onPacketReceive(packet)
+        }
+    }
 
     fun start() {
 
+
+
     }
+
+    fun end() {
+
+    }
+
+    private fun handleRequestProfile() {
+
+    }
+
+    private fun onPacketReceive(combinedPacket: ParsedCombinedPacket) {
+        when (combinedPacket.metaPacket) {
+            is RequestProfilePacket -> {
+
+            }
+        }
+    }
+
+
 
 
 }
