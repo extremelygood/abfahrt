67e06387850d647bf2f4660ff2725d41e070f18f
extremelygood <extremelygood@vacant.com>
2025-07-06 12:49:27 +0200

    Added coroutine magic

--- app/src/main/java/com/extremelygood/abfahrt/network/EncounterHandler.kt
+++ app/src/main/java/com/extremelygood/abfahrt/network/EncounterHandler.kt
@@ -1,7 +1,19 @@
 package com.extremelygood.abfahrt.network
 
+import MatchProfile
 import com.extremelygood.abfahrt.classes.DatabaseManager
 import com.extremelygood.abfahrt.classes.UserProfile
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.runInterruptible
+
+const val MAX_ENCOUNTERS_TO_GET = 50
 
 /**
  * Class for handling a connection made to another device
@@ -10,6 +22,10 @@ class EncounterHandler(
     private val connection: NearbyConnection,
     private val database: DatabaseManager,
 ) {
+    private var myCoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    private var mainJob: Job? = null
+
+
     init {
         connection.setDisconnectCallback {
             end()
@@ -23,11 +39,24 @@ class EncounterHandler(
 
 
     fun start() {
+        end()
 
+        mainJob = myCoroutineScope.launch {
+
+            // Step 1: Exchange profiles
+
+            val encountersDeferred = async { database.getAllMatches(MAX_ENCOUNTERS_TO_GET) }
+            val profileDeferred = async { database.loadMyProfile() }
+
+            val encountersList = encountersDeferred.await()
+            val ownProfile = profileDeferred.await()
+
+        }
     }
 
-    fun end() {
 
+    fun end() {
+        mainJob?.cancel()
     }
 
     private fun handleRequestProfile() {
