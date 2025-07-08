4027215dd3fb119619dfe5aad48fcac4bcea5fa8
extremelygood <extremelygood@vacant.com>
2025-07-08 19:50:40 +0200

    Updated to late init with applicationContext

--- app/src/main/java/com/extremelygood/abfahrt/AbfahrtApplication.kt
+++ app/src/main/java/com/extremelygood/abfahrt/AbfahrtApplication.kt
@@ -1,21 +1,38 @@
 package com.extremelygood.abfahrt
 
 import android.app.Application
+import android.widget.Toast
+import com.extremelygood.abfahrt.classes.DatabaseManager
+import com.extremelygood.abfahrt.network.EncounterHandler
 import com.extremelygood.abfahrt.network.NearbyConnectionManager
 
+const val SERVICE_ID = "com.abfahrt"
+
 class AbfahrtApplication: Application() {
+    lateinit var databaseManager: DatabaseManager
+    lateinit var connectionManager: NearbyConnectionManager
 
     override fun onCreate() {
         super.onCreate()
+
+        databaseManager = DatabaseManager.getInstance(applicationContext)
+        connectionManager = NearbyConnectionManager(applicationContext, SERVICE_ID)
 
         startConnectionManager()
 
     }
 
+
     private fun startConnectionManager() {
-        val connectionManager = NearbyConnectionManager(this, "")
         connectionManager.startDiscovery()
         connectionManager.startAdvertising()
+
+        connectionManager.setOnConnectionEstablished { newConnection ->
+            Toast.makeText(this, "Connection established", Toast.LENGTH_SHORT).show()
+            EncounterHandler(newConnection, databaseManager).start()
+        }
+
+
     }
 
 
