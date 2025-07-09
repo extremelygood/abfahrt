c077865b02928da5574118eb92f5a14d879beede
extremelygood <extremelygood@vacant.com>
2025-07-09 17:41:47 +0200

    Refactored serialization

--- app/src/main/java/com/extremelygood/abfahrt/classes/GeoLocation.kt
+++ app/src/main/java/com/extremelygood/abfahrt/classes/GeoLocation.kt
@@ -7,55 +7,64 @@ import kotlinx.serialization.SerializationException
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.descriptors.buildClassSerialDescriptor
 import kotlinx.serialization.descriptors.element
-import kotlinx.serialization.encoding.CompositeDecoder
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.encoding.CompositeDecoder
+import kotlinx.serialization.encoding.decodeStructure
+import kotlinx.serialization.encoding.encodeStructure
+
 
 @Serializable
-class GeoLocation(
+data class GeoLocation(
     val locationName: String = "DefaultLocationName",
 
     @Serializable(with = AndroidLocationSerializer::class)
-    val location: Location = Location(null)
+    val location: Location = Location(PROVIDER_MANUAL)
 ) {
+
+    companion object {
+        const val PROVIDER_MANUAL = "manual"
+    }
 }
 
 
 object AndroidLocationSerializer : KSerializer<Location> {
+    private const val LAT_INDEX = 0
+    private const val LON_INDEX = 1
+
     override val descriptor: SerialDescriptor =
         buildClassSerialDescriptor("Location") {
             element<Double>("latitude")
             element<Double>("longitude")
         }
 
+    override fun serialize(encoder: Encoder, value: Location) =
+        encoder.encodeStructure(descriptor) {
+            encodeDoubleElement(descriptor, LAT_INDEX, value.latitude)
+            encodeDoubleElement(descriptor, LON_INDEX, value.longitude)
+        }
 
+    override fun deserialize(decoder: Decoder): Location =
+        decoder.decodeStructure(descriptor) {
+            var lat: Double? = null
+            var lon: Double? = null
 
-    override fun deserialize(decoder: Decoder): Location {
-        val dec = decoder.beginStructure(descriptor)
+            while (true) when (decodeElementIndex(descriptor)) {
+                LAT_INDEX  -> lat = decodeDoubleElement(descriptor, LAT_INDEX)
+                LON_INDEX  -> lon = decodeDoubleElement(descriptor, LON_INDEX)
+                CompositeDecoder.DECODE_DONE -> break
+                else -> throw SerializationException("Unexpected element index")
+            }
 
-        var lat = 0.0
-        var lon = 0.0
+            if (lat == null || lon == null) {
+                throw SerializationException("Missing latitude and/or longitude")
+            }
 
-        loop@ while (true) {
-            when (val index = dec.decodeElementIndex(descriptor)) {
-                CompositeDecoder.DECODE_DONE -> break@loop
-                0 -> lat = dec.decodeDoubleElement(descriptor, 0)
-                1 -> lon = dec.decodeDoubleElement(descriptor, 1)
-                else -> throw SerializationException("Unexpected index $index")
+            Location(GeoLocation.PROVIDER_MANUAL).apply {
+                latitude  = lat
+                longitude = lon
             }
         }
-        dec.endStructure(descriptor)
 
-        return Location("").apply {
-            latitude = lat
-            longitude = lon
-        }
-    }
 
-    override fun serialize(encoder: Encoder, value: Location) {
-        val struct = encoder.beginStructure(descriptor)
-        struct.encodeDoubleElement(descriptor, 0, value.latitude)
-        struct.encodeDoubleElement(descriptor, 1, value.longitude)
-        struct.endStructure(descriptor)
-    }
 }
