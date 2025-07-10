a11c4d0ccae663f87b9f1604071f6c36cc30af64
extremelygood <extremelygood@vacant.com>
2025-07-10 18:18:10 +0200

    Added map marker for match

--- app/src/main/java/com/extremelygood/abfahrt/ui/match/MatchFragment.kt
+++ app/src/main/java/com/extremelygood/abfahrt/ui/match/MatchFragment.kt
@@ -1,6 +1,7 @@
 package com.extremelygood.abfahrt.ui.match
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,13 +14,21 @@ import com.extremelygood.abfahrt.databinding.FragmentMatchBinding
 import com.extremelygood.abfahrt.ui.profile.ProfileViewModel
 import com.extremelygood.abfahrt.ui.viewModelFactory
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlin.getValue
 
 class MatchFragment : Fragment() {
 
     private var _binding: FragmentMatchBinding? = null
     private val binding get() = _binding!!
+
+    private var googleMap: GoogleMap? = null
+    private var matchDestinationMarker: Marker? = null
+
 
     private val matchViewModel: MatchViewModel by viewModels {
         viewModelFactory {
@@ -50,8 +59,9 @@ class MatchFragment : Fragment() {
             val defaultLatLng = LatLng(52.520008, 13.404954)
             val defaultZoomLvl = 10f
 
-            // Kamera setzen, keine Marker oder Klicklistener
+            // Kamera setzen oder Klicklistener
             googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultLatLng, defaultZoomLvl))
+            this.googleMap = googleMap
         }
 
         matchViewModel.matchProfile.observe(viewLifecycleOwner) { bestMatch ->
@@ -85,11 +95,48 @@ class MatchFragment : Fragment() {
 
     }
 
+
+    private fun setMatchDestinationMarker(newLatLng: LatLng?) {
+        if (googleMap == null) {
+            return
+        }
+
+        matchDestinationMarker?.remove()
+        if (newLatLng == null) {
+            return
+        }
+
+
+        val markerOptions = MarkerOptions().apply {
+            position(newLatLng)
+            title("Match Destination")
+            icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+        }
+
+        matchDestinationMarker = googleMap!!.addMarker(markerOptions)
+
+        val cameraUpdate = CameraUpdateFactory.newLatLng(newLatLng)
+        googleMap!!.animateCamera(cameraUpdate)
+    }
+
+
     private fun displayMatch(matchToDisplay: MatchProfile) {
         binding.firstNameText.setText(matchToDisplay.firstName)
         binding.lastNameText.setText(matchToDisplay.lastName)
-        binding.ageField.setText(matchToDisplay.age)
+
+        var ageAsString: String
+        try {
+            ageAsString = matchToDisplay.age.toString()
+        } catch(e: Exception) {
+            Log.d("MatchFragment", "Exception while trying to convert age to string")
+            ageAsString = ""
+        }
+
+        binding.ageField.setText(ageAsString)
         binding.descriptionField.setText(matchToDisplay.description)
+
+        val newLatLng = LatLng(matchToDisplay.destination.location.latitude, matchToDisplay.destination.location.longitude)
+        setMatchDestinationMarker(newLatLng)
     }
 
     private fun onBestMatchChanged(bestMatch: MatchProfile?) {
