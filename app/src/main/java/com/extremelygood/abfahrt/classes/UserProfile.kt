6743da9190b4243e0322988ec399292ae3177152
extremelygood <extremelygood@vacant.com>
2025-07-10 18:19:27 +0200

    Changed default strings

--- app/src/main/java/com/extremelygood/abfahrt/classes/UserProfile.kt
+++ app/src/main/java/com/extremelygood/abfahrt/classes/UserProfile.kt
@@ -9,10 +9,10 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "user_profile")
 data class UserProfile(
     @PrimaryKey val id: String = "me",
-    var firstName: String = "DefaultFirstName",
-    var lastName: String = "DefaultLastName",
+    var firstName: String = "",
+    var lastName: String = "",
     var age: Int = -1,
-    var description: String = "DefaultDescription",
+    var description: String = "",
     @Embedded var destination: GeoLocation = GeoLocation(),
     var isDriver: Boolean = false,
 )
